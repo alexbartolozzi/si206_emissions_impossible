5d9b2d1844152166fdcb79003e0c648c25b25d78
Alex Bartolozzi <alexbart@umich.edu>
2024-12-15 17:53:58 -0500

    shell script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run the application
+source env/bin/activate
+#TODO: adjust this for count of celebs we fetch
+# python main.py new 0
+# for i in {1..63}
+# do
+#   python3 main.py _ $i
+# done
+
+echo "python main.py new 0"
+for i in {1..63}
+do
+  echo "python main.py _ $i"
+done
